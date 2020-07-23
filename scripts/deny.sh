4cadd592acccb40ab1cc97f068e07403259505a7
dotjava <wisenow@163.com>
2020-07-23 11:02:35 +0800

    修改 reject 为drop

--- scripts/deny.sh
+++ scripts/deny.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-firewall-cmd --permanent --add-rich-rule="rule family=ipv4 source address=$1 port protocol=tcp port=80 reject"
+firewall-cmd --permanent --add-rich-rule="rule family=ipv4 source address=$1 port protocol=tcp port=80 drop"
 #  --add-rich-rule='rule family=\"ipv4\" source address=\"$1\" port protocol=\"tcp\" port=\"80\"\' reject"
 # echo $cmd
 # out=`$cmd`
